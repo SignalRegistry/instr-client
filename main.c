7d21ab943d449b9f150f328fd777a721eeb1208f
Hüseyin <iletisim@huseyinyigit.com.tr>
2021-01-17 13:44:06 +0300

    remove request (libcurl) routines

--- main.c
+++ main.c
@@ -2,11 +2,12 @@
 #include <tchar.h>
 #include <stdio.h>
 
-#include <curl/curl.h>
-
+#define USERID "DE455gfdd"
 #define INFO_BUFFER_SIZE 100
+
 void printError(TCHAR* msg);
-size_t write_data(void* buffer, size_t size, size_t nmemb, void* userp);
+
+
 void main()
 {
 	/*
@@ -41,31 +42,8 @@ void main()
 	strcpy_s(userName, sizeof userName, infoBuf);
 
 	// register
-	printf("\n-> Registering client with computer and user name ...");
-	_tprintf(TEXT("\n    - Computer name: %s"), computerName);
-	_tprintf(TEXT("\n    - User name    : %s"), userName);
-	
-	CURL* curl;
-	CURLcode res;
+	printf("\n-> Connecting Signal Registry\xa9 with user id %s ...", USERID);
 
-	curl = curl_easy_init();
-	if (curl) {
-		curl_easy_setopt(curl, CURLOPT_URL, "https://app.instr.signalregistry.net");
-		/* example.com is redirected, so we tell libcurl to follow redirection */
-		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-
-		/* Perform the request, res will get the return code */
-		res = curl_easy_perform(curl);
-
-		/* Check for errors */
-		if (res != CURLE_OK)
-			fprintf(stderr, "curl_easy_perform() failed: %s\n",
-				curl_easy_strerror(res));
-
-		/* always cleanup */
-		curl_easy_cleanup(curl);
-	}
 	
 }
 
@@ -91,10 +69,4 @@ void printError(TCHAR* msg)
 
 	// Display the message
 	_tprintf(TEXT("\n\t%s failed with error %d (%s)"), msg, eNum, sysMsg);
-}
-
-size_t write_data(char* buffer, size_t size, size_t nmemb, void* userp)
-{
-	printf("\nResponse: %s", buffer);
-	return size * nmemb;
 }
